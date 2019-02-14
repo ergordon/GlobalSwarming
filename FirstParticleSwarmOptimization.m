297cdc35250b2c3a8d7d432546cf23a26c56f0a2
kwanchangnim <stierint@hotmail.com>
2019-02-13 22:36:53 -0600

    Update FirstParticleSwarmOptimization.m

--- FirstParticleSwarmOptimization.m
+++ FirstParticleSwarmOptimization.m
@@ -1,170 +1,157 @@
-%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Emilio and David
 %this is our first attempt at implementing a swarm intellegence algorithm
 %this work was started on 2/11/2019
 %we are implementing the basic particle swarm algorithm found in
 %Evolutionary and Swarm Intelligence Algorithms.
-%we will also optimize the example given in that pdf.
-
-clc; clear; close all;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Begin Input Parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-maxIterationsPerImprovement = 100; %the maximum number of iterations after finding a new global best. This is the stopping criteria. Note: A different stopping criteria could be defined.
-g = 0; %the index of the best particle in the swarm
-c1 = .2; %cognative scaling parameter. determines the speed at which each particle moves towards its previous best position
-c2 = .2; %social scaling parameter. determines the speed at which each particle moves towards the global best position
-%{
-note about these scaling parameters
-In basic version of PSO, c1 = c2 = 2 were
-chosen. With this choice, particle’s speed increases without control which is good
-for faster convergence rate but harmful for better exploitation of the search space.
-If we set c1 = c2 > 0 then particles will attract towards the average of pbest and
-gbest. c1 > c2 setting will be beneficial for multimodal problems while c2 > c1 will
-be beneficial for unimodal problems. Small values of c1 and c2 will provide smooth
-particle trajectories during the search procedure while larger values of c1 and c2 will
-be responsible for abrupt movements with more acceleration.
-%}
-
-
-%example from book.
-%Minf(x1,x2) = x1^2 + x2^2; where x1,x2 are in the range [-5,5]
-D = 2;
-SbyDim = [5,4]; % particles along the 1st dimension, 4 along the second dimension. total of 20 particles
-S = 1;  
-for i=1:D
-    S = S*SbyDim(i);
-end
-ranges = [-5 5;  %search space for first dimension
-          -4 4]; %search space for second dimension
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% End Input Parameters
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-      
+% This is a basic particle swarm optimization implementation based on 
+% Evolutionary and Swarm Intellegence Algorithms published by Springer in 
+% Studies of Computational Intelligence Volume 779
+%
+% ::Inputs::
+% -objectiveFunction-
+% this is the function that your your are trying to minimize. It should be 
+% declared as something like obfunc = @(x) x(1)^2 + x(2)^2 + x(3)^2;
+% -dimension-
+% this is a matrix of the bounds on the search space for each variable. it
+% should be declared as something like domain = [-1 1; -2 4; -8 8];
+% each row represents the search space for each deimension. so in the
+% example above, x(1) is restriced between [-1 1]. Also, the number of rows
+% must match the number variables being optimized.
+% -numParticles-
+% this is the number of particles that belong to the swarm. Their initial
+% positions are normally distribued about each dimension.
+% -c1-
+% cognative scaling parameter. determines the speed at which each particle 
+% moves towards its previous best position.
+% -c2-
+% social scaling parameter. determines the speed at which each particle 
+% moves towards the global best position
+% (note about scaling parameters c1 and c2)
+% In basic version of PSO, c1 = c2 = 2 were chosen. With this choice, 
+% particleâ€™s speed increases without control which is good for faster 
+% convergence rate but harmful for better exploitation of the search space.
+% If we set c1 = c2 > 0 then particles will attract towards the average of 
+% pbest and gbest. c1 > c2 setting will be beneficial for multimodal 
+% problems while c2 > c1 will be beneficial for unimodal problems. Small 
+% values of c1 and c2 will provide smooth particle trajectories during the 
+% search procedure while larger values of c1 and c2 will be responsible for 
+% abrupt movements with more acceleration.
+% -maxIterations-
+% the number of iterations to execute before stopping the search
+% -v_max_percent-
+% a scale on the upper velocity bound for each particle in each dimension.
+% This just scales the length of the dimension by the value passed in. You
+% want to keep this below 0.5 and probably even much lower than that.
+%
+% ::outputs::
+% -gbest_position-
+% the set of arguments(x1, x2, ... , xn) that optimize the objective funtion
+% -gbest_value-
+% the value returned when evaluating the objective function with
+% gbest_position
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [gbest_position, gbest_value] = PSO(objectiveFunction, dimension, numParticles, c1, c2, iterations, v_max_percent)
+  
+   
+
       
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Begin Initialization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       
-      
+%number of dimensions in the search space    
+D = size(dimension,1); 
 %initialize the position matrix
-x = zeros(S,D);
+x = zeros(numParticles,D);
 %initialize the velocity matrix
-v = zeros(S,D);
+v = zeros(numParticles,D);
 %initialize the max velicty matrices
 v_max = zeros(D,1);
 %initialize the pbest position matrix
-pbest_position = zeros(S,D);
+pbest_position = zeros(numParticles,D);
 %initialize the pbest value matrix
-pbest_value = zeros(S,1);
+pbest_value = zeros(numParticles,1);
 %initialize the gbest position matrix
 gbest_position = zeros(1,D);
 %initialize the gbest value
-gbest_value = nan;
+gbest_value = inf;
 
-
-%TODO actually implement this
-%{
-%initialize the initial positions
-%lets equally space them in a grid and then add a bit of noise to randomize
-%it a bit. Scale the noise by the grid size in some fasion.
-
-%for each dimension
-for d1=1:D 
-
-    %determine how far to space each particle by dividing the length of the
-    %range by the dimension of particles
-    step_size = (ranges(d1,end) - ranges(d1,1))/(SbyDim(d1)+1);
-    
-    %TODO make this work beyond the 2D case....
-    %for each 'column' of particles
-    for s1=1:SbyDim(d1)
-        
-        %for each other dimension
-        for d2=1:D
-           if(d2~=d1)
-               %for each 'row' of particles
-                for s2=1:SbyDim(d2)
-                    if(s2~=s1)
-                    
-                        noise = normrnd(0,step_size/6);
-                        x(i,d) = step_size*i + noise;
-                        
-                    end
-                end
-           end
-        end
-        
-        
-        
-%         %create noise scaled with a standard deviation 1/6th of the
-%         %stepsize size and apply it to the equally spaced position
-%         noise = normrnd(0,step_size/6);
-%         x(i,d) = step_size*i + noise;
-        
-    end
-end
-%}
 
 %initialize the initial posisions
-%use a random distribution bounded by the range of each dimension
-
-
+%use a random distribution bounded by the length of each dimension
 
 %for each dimension
 for d=1:D
     
     %get the length of the dimension
-    range_size = ranges(d,end) - ranges(d,1);
-
+    dimension_size = dimension(d,end) - dimension(d,1);
+    dimension_center = dimension_size/2;
+    
     %for each particle
-    for i=1:S
+    for i=1:numParticles
 
-        %create noise scaled with a standard deviation 1/6th of the
-        %dimension range size and apply
-        x(i,d) = normrnd(0,range_size/6);
+        %normally distribute each particle about each dimension center with
+        %a standard deviation 1/6th of the dimension length
+        x(i,d) = normrnd(dimension_center,dimension_size/6);
 
     end
 end
 
-figure();
-title('PSO for minf(x1,x2)=x1^{2}+x2^{2}');
-xlabel('x-position (m)');
-ylabel('y-position (m)');
-vid_writer = VideoWriter('PSO.avi');
-vid_writer.FrameRate = 5;
-open(vid_writer);
-axis([ranges(1,1) ranges(1,2) ranges(2,1) ranges(2,2)]);
-hold on;
-plotHandlePos = zeros(S,1);
-plotHandleVel = zeros(S,1);
-
-
-
+%visualize swarm if in two dimensions
+if(D == 2)
+    figure();
+    title('PSO Visualization in 2D');
+    xlabel('x-position (m)');
+    ylabel('y-position (m)');
+    vid_writer = VideoWriter('PSO_2D.avi');
+    vid_writer.FrameRate = 5;
+    open(vid_writer);
+    axis([dimension(1,1) dimension(1,2) dimension(2,1) dimension(2,2)]);
+    hold on;
+    plotHandlePos = zeros(numParticles+1,1); % one more for gbest
+    plotHandleVel = zeros(numParticles,1);
+end
+%visualize swarm if in three dimensions
+if(D == 3)
+    figure();
+    title('PSO Visualization in 3D');
+    xlabel('x-position (m)');
+    ylabel('y-position (m)');
+    xlabel('z-position');
+    vid_writer = VideoWriter('PSO_3D.avi');
+    vid_writer.FrameRate = 5;
+    open(vid_writer);
+    axis([dimension(1,1) dimension(1,2) dimension(2,1) dimension(2,2) dimension(3,1) dimension(3,2)]);
+    hold on;
+    plotHandlePos = zeros(numParticles+1,1); % one more for gbest
+    plotHandleVel = zeros(numParticles,1);
+end
 
 %initialize the maximum velocity matrix
-%this will set the maximum velocity to be half of the lenght of each
-%dimension
+%this will set the maximum velocity to be the given percent of the length
+%of each dimension
 
 %for each dimension
 for d=1:D
     
     %find vmax by dividing the length of the range by 2
-    v_max(d) = (ranges(d,end) - ranges(d,1))/2;
+    v_max(d) = abs((dimension(d,end) - dimension(d,1))*v_max_percent);
     
 end
 
-%initialize the velocity matix
+%initialize the velocity matrix
 %use a normal distribution that keeps it within the range [-v_max, v_max]
 for d=1:D
    
     %for each particle
-    for i=1:S
+    for i=1:numParticles
         
         %set the velocity to be noise scaled with a standard deviation 
         %1/3th of the max veliocity
@@ -173,35 +160,47 @@ for d=1:D
     end
 end
 
-
-%plot the initial positions and velocities
-for i=1:S
-       plotHandlePos(i) = plot(x(i,1),x(i,2),'o');
-       plotHandleVel(i) = plot([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)]);
+%visualize swarm if in two dimensions
+if(D == 2)
+    %plot the initial positions and velocities
+    for i=1:numParticles
+           plotHandlePos(i) = plot(x(i,1),x(i,2),'o');
+           plotHandleVel(i) = plot([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)]);
+    end
+    plotHandlePos(end) = plot(gbest_position(1),gbest_position(2),'o');
+    frame = getframe(gcf);
+    writeVideo(vid_writer,frame);
+    delete(plotHandlePos);
+    delete(plotHandleVel);
 end
-
-frame = getframe(gcf);
-writeVideo(vid_writer,frame);
+%visualize swarm if in three dimensions
+if(D == 3)
+    %plot the initial positions and velocities
     
-delete(plotHandlePos);
-delete(plotHandleVel);
-
-    
-%initializing the gbest postion and value 
-%use the first particle to start
-gbest_position = x(1,1:end);
-gbest_value = objectiveFunction(gbest_position)
+    for i=1:numParticles
+           plotHandlePos(i) = plot3(x(i,1),x(i,2),x(i,3),'o');
+           plotHandleVel(i) = plot3([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)],[x(i,3) x(i,3)+v(i,3)]);
+    end
+    plotHandlePos(end) = plot3(gbest_position(1),gbest_position(2),gbest_position(3),'o');
+    frame = getframe(gcf);
+    writeVideo(vid_writer,frame);
+    delete(plotHandlePos);
+    delete(plotHandleVel);
+end
 
 %initialize pbest position and value
 %evaluate the objective function for each particle position.
-for i=1:S
+for i=1:numParticles
     
-   pbest_position(i,:) = x(i,:)
-   pbest_value(i) = objectiveFunction(pbest_position(i,:))
-    
+   pbest_position(i,:) = x(i,:);
+   pbest_value(i) = objectiveFunction(pbest_position(i,:));
+
+   %find the initial gbest parameters
+   if(pbest_value(i) < gbest_value)
+        gbest_position = pbest_position(i,:);
+        gbest_value = pbest_value(i);
+   end
 end
-
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % End Initialization
@@ -211,26 +210,11 @@ end
 % Begin Main Algorithm
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-search = true;
-counter = 0;
 %for each time step
-% for t=1:maxIterations %TODO replace with while and convergence criteria
+for t=1:iterations
 
-%for each iteration
-while(search)
-    
-    %increase the counter each update
-    counter = counter + 1;
-    
-    %if we haven't found a new best solution before the counter gets to the
-    %set max, stop the search
-    if(counter >= maxIterationsPerImprovement)
-        search = false;
-    end
-    
-    
     %for each particle
-    for i=1:S 
+    for i=1:numParticles
         %for each dimension of the search space
         for d=1:D
             
@@ -246,131 +230,99 @@ while(search)
             x(i,d) = x(i,d) + v(i,d);
             
             %keep the position within the searchspace.
-            [pos,vel] = boundPositionVelocity(x(i,d), v(i,d), ranges(d,:));
+            [pos,vel] = boundPositionVelocity(x(i,d), v(i,d), dimension(d,:), v_max(d));
             
             x(i,d) = pos;
             v(i,d) = vel;
         end
         
-        %update fitness
-        
         %evaluate objective function 
         obj_value = objectiveFunction(x(i,:));
        
-        note = 'check'
-        ov = obj_value
-        pbv = pbest_value(i)
-        %update pbest
+        %update pbest if better
         if(obj_value < pbest_value(i))
-           note = 'update'
            pbest_position(i,:) = x(i,:);
            pbest_value(i) = obj_value;
         end
         
-        %update gbest
+        %update gbest if better
         if(obj_value < gbest_value)
-          
-           %we found a new best, reset the iteration coutner;
-           counter = 0;
-            
-           %store the result
            gbest_position = x(i,:);
-           gbest_value = obj_value
+           gbest_value = obj_value;
         end    
     end
     
-    
-    %plot the positions
-    for i=1:S
-           plotHandlePos(i) = plot(x(i,1),x(i,2),'o');
-           plotHandleVel(i) = plot([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)]);
+    %visualize swarm if in two dimensions
+    if(D == 2)
+        %plot the positions
+        for i=1:numParticles
+               plotHandlePos(i) = plot(x(i,1),x(i,2),'o');
+               plotHandleVel(i) = plot([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)]);
+        end
+        plotHandlePos(end) = plot(gbest_position(1),gbest_position(2),'o');
+        frame = getframe(gcf);
+        writeVideo(vid_writer,frame);
+        delete(plotHandlePos)
+        delete(plotHandleVel)
     end
-
-    frame = getframe(gcf);
-    writeVideo(vid_writer,frame);
-    
-    delete(plotHandlePos)
-    delete(plotHandleVel)
+    %visualize swarm if in three dimensions
+    if(D == 3)
+        %plot the positions
+        for i=1:numParticles
+               plotHandlePos(i) = plot3(x(i,1),x(i,2),x(i,3),'o');
+               plotHandleVel(i) = plot3([x(i,1) x(i,1)+v(i,1)],[x(i,2) x(i,2)+v(i,2)],[x(i,3) x(i,3)+v(i,3)]);
+        end
+        plotHandlePos(end) = plot3(gbest_position(1),gbest_position(2),gbest_position(3),'o');
+        frame = getframe(gcf);
+        writeVideo(vid_writer,frame);
+        delete(plotHandlePos)
+        delete(plotHandleVel)
+    end
 end
 
-close(vid_writer);
-
-gbest_position
-gbest_value
+%visualize swarm if in two dimensions
+if(D == 2)
+    close(vid_writer);
+end
+%visualize swarm if in three dimensions
+if(D == 3)
+    close(vid_writer);
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % End Main Algorithm
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Begin Objective Function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%this is the function that we are trying to optimize.
-%in our case we are using the example funciton from book.
-%Minf(x1,x2) = x1^2 + x2^2;
-function result = objectiveFunction(position)%TODO consider a better name than position...
-
-    %evaluate the objective function at this position
-    result = position(1)^2 + position(2)^2;     
-        
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% End Objective Function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Begin Bounding Function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %keep each particle within the bounds of the search space. If the particle
 %goes outside of the search space, set it to the boundary value and set the
 %velocity to zero.
-% function [boundedPosition,boundedVelocity] = boundPositionVelocity(position, velocity, bounds)
-% 
-%     %for each particle
-%     for i=1:size(position,1)
-%         %for each dimension
-%         for d=1:size(bounds,1)
-%             
-%             %enforce upper bound
-%             if(position(i,d) > bounds(d))
-%                position(i,d) = bounds(d);
-%                velocity(i,d) = 0;
-%             end
-%             %enforce lower bound
-%             if(position(i,d) < bounds(d))
-%                position(i,d) = bounds(d);
-%                velocity(i,d) = 0;
-%             end
-%             
-%         end
-%         
-%     end
-% 
-%     %return the bounded position and velocity
-%     boundedPosition = position;
-%     boundedVelocity = velocity;
-%     
-% end
-
-function [boundedPosition,boundedVelocity] = boundPositionVelocity(position, velocity, bounds)
+function [boundedPosition,boundedVelocity] = boundPositionVelocity(position, velocity, bounds, max_velocity)
 
     maxbound = max(bounds);
     minbound = min(bounds);
 
-    %enforce upper bound
+    %enforce upper bound on position
     if(position > maxbound)
        position = maxbound;
        velocity = 0;
     end
-    %enforce lower bound
+    %enforce lower bound on position
     if(position < minbound)
        position = minbound;
        velocity = 0;
+    end
+    
+    %enforce upper bound on velocity
+    if(velocity > max_velocity)
+       velocity = max_velocity;
+    end
+    %enforce lower bound on velocity
+    if(velocity < -max_velocity)
+       velocity = -max_velocity;
     end
     
     %return the bounded position and velocity
@@ -378,11 +330,4 @@ function [boundedPosition,boundedVelocity] = boundPositionVelocity(position, vel
     boundedVelocity = velocity;
     
 end
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% End Bounding Function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
